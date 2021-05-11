3b0a8200e0ab8268fa0bd946cb16170d01a86cd9
Winston maben <72695682+Winstonmn99@users.noreply.github.com>
2021-05-11 15:42:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import pickle
+import streamlit as st
+
+pickle_in = open('real_model.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+
+@st.cache()
+
+# defining the function which will make the prediction using the data which the user inputs
+def prediction(Engine_rpm,Shaft_power,Wind_speed,Current_speed):
+
+     prediction = classifier.predict( [[Engine_rpm,Shaft_power,Wind_speed,Current_speed]])
+     print(prediction)
+     return prediction
+
+
+def main():
+    # front end elements of the web page
+    html_temp = """
+    <div style ="background-color:yellow;padding:13px">
+    <h1 style ="color:black;text-align:center;">Predictive Modelling of Ship Fuel Consumption using Machine Learning</h1>
+    </div>
+    """
+
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html = True)
+
+    # following lines create boxes in which user can enter data required to make prediction
+    Engine_rpm = st.number_input('Engine rpm')
+    Shaft_power = st.number_input('Shaft power')
+    Wind_speed = st.number_input("Wind speed")
+    Current_speed = st.number_input("Current speed")
+    result =""
+
+
+    if st.button("Predict"):
+        result = prediction(Engine_rpm,Shaft_power,Wind_speed,Current_speed)
+        st.success('The required fuel rate is {}'.format(result))
+        print(result)
+
+if __name__=='__main__':
+    main()
